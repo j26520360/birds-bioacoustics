8d19ddf33896c03908e9600bdf06fda3a747862e
j26520360 <153921990+j26520360@users.noreply.github.com>
2026-01-11 10:38:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,66 @@
+import io
+import os
+import tempfile
+
+import bioacoustics_model_zoo as bmz
+import pandas as pd
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+def main():
+    files = st.file_uploader(
+        "Upload",
+        type="wav",
+        accept_multiple_files=True,
+    )
+
+    samples = []
+    for file in files:
+        with tempfile.NamedTemporaryFile(
+            suffix=".wav",
+            prefix=file.name,
+            delete=False,
+        ) as sample:
+            sample.write(file.read())
+            samples.append(sample.name)
+
+    container = st.container(horizontal=True)
+
+    run = container.button(
+        "Run",
+        disabled=not samples,
+    )
+
+    if run:
+        with st.spinner():
+            model = get_model()
+            scores = model.predict(samples, activation_layer="softmax")
+            scores = scores.idxmax(axis=1)
+            scores.name = "label"
+
+            data = io.BytesIO()
+            with pd.ExcelWriter(data) as writer:
+                scores.to_excel(writer)
+                data.seek(0)
+
+    container.download_button(
+        "Download",
+        data=data if run else "",
+        file_name="birds.xlsx",
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        on_click="ignore",
+        type="primary",
+        disabled=not run,
+    )
+
+    for sample in samples:
+        os.remove(sample)
+
+
+@st.cache_resource
+def get_model():
+    return bmz.Perch2()
+
+
+if __name__ == "__main__":
+    main()
